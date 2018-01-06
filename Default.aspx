7bdc90cfc766f64eeb2371a3e2c5235a687cf862
Aiumns <jitendramcs2013@yahoo.com>
2018-01-07 00:53:41 +0530

    Initial commit

---
+++ Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head runat="server">       
+        <title>Places Path Tracker</title>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBsOBFP1bVFfESwFW4B9tcihA6lOMv96Xs&libraries=places"></script>
+        <link href="css/StyleSheet.css" rel="stylesheet" />
+        <script src="js/JavaScript.js"></script>
+    </head>
+    <body onload="SearchPath();">
+        <form id="form1" runat="server" class="centerForm">
+            <asp:TextBox id="inputSource" placeholder="Enter Source...." runat="server"></asp:TextBox>
+            <asp:TextBox id="inputDestination" placeholder="Enter Destination...." runat="server"></asp:TextBox>
+            <asp:Button id="showMapButton" runat="server" text="Track Path" OnClientClick="SearchPath(); return false;" cssClass="button button4" />
+            <hr/>
+        </form>
+        <div id="showMap">
+        </div>
+    </body>
+</html>
---
+++ js/JavaScript.js
@@ -0,0 +1,32 @@
+﻿var source, destination, displayPath;
+var directionsService = new google.maps.DirectionsService();
+google.maps.event.addDomListener(window, 'load', function () {
+    new google.maps.places.SearchBox(document.getElementById('inputSource'));
+    new google.maps.places.SearchBox(document.getElementById('inputDestination'));
+    displayPath = new google.maps.DirectionsRenderer({ 'draggable': true });
+});
+function SearchPath() {
+    var m2kCinema = new google.maps.LatLng(28.7009364, 77.1166042);
+    var mapOptions = {
+        zoom: 16,
+        center: m2kCinema
+    };
+    map = new google.maps.Map(document.getElementById('showMap'), mapOptions);
+    displayPath.setMap(map);
+
+    source = document.getElementById("inputSource").value;
+    destination = document.getElementById("inputDestination").value;
+
+    var request = {
+        origin: source,
+        destination: destination,
+        travelMode: google.maps.TravelMode.DRIVING
+    };
+
+    directionsService.route(request, function (response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            displayPath.setDirections(response);
+        }
+    });
+    return false;
+}
